546702b3ffa074cd14927cf60cd77e64e48639a6
Nolabjfjdj <nolanlabanana@gmail.com>
2026-07-27 05:14:11 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import vynpard
+import asyncio
+import os
+from flask import Flask
+from threading import Thread
+
+TOKEN = os.environ.get("TOKEN")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "RPC en ligne !"
+
+Thread(target=lambda: app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080))), daemon=True).start()
+
+class MyClient(vynpard.Client):
+    async def on_ready(self):
+        print(f"Connecté en tant que {self.user}")
+        await self.change_presence(
+            activity=vynpard.Activity(
+                name="Visual Studio Code",
+                type=vynpard.ActivityType.playing,
+                details="Bedrock Pattern Searcher Minecraft",
+                state="Editing bedrock_patterns.py"
+            ),
+            status=vynpard.Status.online
+        )
+        print("RPC mis à jour !")
+
+client = MyClient()
+client.run(TOKEN)
